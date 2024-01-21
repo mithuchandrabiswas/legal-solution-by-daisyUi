9189c8ac1df1292d22e49c239ee3f3376db6deee
Mithu Chandra Biswas <biswasmithumcb@gmail.com>
2024-01-22 00:27:21 +0600

    Updated

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      colors: {
+        // ...
+        'tahiti': {
+          light: '#67e8f9',
+          DEFAULT: '#06b6d4',
+          dark: '#0e7490',
+        },
+        // ...
+      },
+    },
+  }
